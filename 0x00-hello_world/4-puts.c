a62d1b79bace514b45681d84f7862ca963f46565
Your Name <you@example.com>
2023-06-15 16:10:27 -0700

    aa

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main - Write a C program that prints exactly "Programming is like building a multilingual puzzle
+ * main - prints exactly "Programming is like building a multilingual puzzle
  * Return: 0 (success)
  */
 
-int main (void)
+int main(void)
+
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);

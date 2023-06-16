907e70d72e48ca62a9610d2dba748f7f2bb377dc
Your Name <you@example.com>
2023-06-16 10:41:33 -0700

    ee

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Write a program that prints the alphabet
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int n , a;
+
+	for (n = 97; n <= 122; n++)
+	{
+		putchar(n);
+	}
+	for (a = 65; a <= 90; a++)
+	{
+	putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}

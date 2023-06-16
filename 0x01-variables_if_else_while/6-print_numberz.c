0cfbcd737e018198e54f2482abe9764b0fd271d1
Your Name <you@example.com>
2023-06-16 11:00:36 -0700

    ee

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints all single digit numbers of base 10
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}

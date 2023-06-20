bda4a7095dc5bdf985df86b09d45d99ca0d28bd0
Your Name <you@example.com>
2023-06-20 09:15:55 -0700

    aaa

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * print_sign - Write a function that prints the sign of a number.
+ * @n: integer 1 or 0 or -1
+ * Return: 1 or 0 ot -1
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else
+	if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}

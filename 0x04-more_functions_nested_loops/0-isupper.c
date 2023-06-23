7a682e9ee90b8d72bb4e26166fef5af17d236616
Your Name <you@example.com>
2023-06-22 17:43:05 -0700

    eee

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/*
+ * _isupper - Write a function that checks for uppercase character.
+ * @c: verify if it s an uppercasa
+ * Return: 0 or 1
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+	return (1);
+	}
+	else
+	return (0);
+}

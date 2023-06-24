ab72d25521930983958d2abbe11d170c8f8cfd37
Your Name <you@example.com>
2023-06-23 18:44:38 -0700

    aa

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,6 +11,12 @@ void print_square(int size)
 	int i;
 	int j;
 
+	if (size <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
 	for (i = 0; i < size; i++)
 	{
 	for (j =  0; j < size; j++)
@@ -19,5 +25,5 @@ void print_square(int size)
 	}
 	_putchar('\n');
 	}
-
+	}
 }

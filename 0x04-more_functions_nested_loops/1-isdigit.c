82d9732cc41b7b2d3716d89fc9ce8fa6b60099e6
Your Name <you@example.com>
2023-06-23 17:13:13 -0700

    hhh

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/*
+ * _isdigit - Write a function that checks for a digit
+ * @c: digits type integer
+ * Return: to 0 or 1
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+	return (1);
+	}
+	return (0);
+}

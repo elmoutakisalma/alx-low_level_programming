a041e0ca78935fc72dadd6b2412f51626d7f3b61
Your Name <you@example.com>
2023-06-27 07:32:42 -0700

    EE

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - function that prints a string, in reverse
+ * @s: string
+ * Return: 0 success
+ */
+
+void print_rev(char *s)
+{
+	int len = 0;
+	int i;
+
+	while (*s != '\0')
+	{
+	len++;
+	s++;
+	}
+	s--;
+	for (i = 0; i <= len; i++)
+	{
+	_putchar(*s);
+	s--;
+	}
+	_putchar('\n');
+}

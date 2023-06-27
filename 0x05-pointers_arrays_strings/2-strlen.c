353f92ec28b275b578ec4ff3f033602b5ca9865a
Your Name <you@example.com>
2023-06-27 07:00:57 -0700

    ee

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - Write a function that returns the length of a string.
+ * @s: string
+ * Return: The lenght of string
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
